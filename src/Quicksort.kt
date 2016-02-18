f4e5b908cda1c67abf066dd98e808df7eb8cb740
Anton Oellerer <aoellerer@gmail.com>
2016-02-18 02:07:07 +0100

    implemented quicksort
    who would have thought the List/array was the hard part?

--- src/Quicksort.kt
+++ src/Quicksort.kt
@@ -1,3 +1,16 @@
 fun main(args: Array<String>) {
-
+    val toSort = intArrayOf(1, 6, 8, 3, 4, 2, 1, 5)
+    print(quicksort(toSort).joinToString())
 }
+
+fun quicksort(toSort: IntArray): IntArray {
+    val pivot = toSort[toSort.size / 2]
+    var smaller = toSort.filter { it < pivot }
+    var bigger = toSort.filter { it > pivot }
+    val smallerList = arrayListOf(smaller)
+    return smaller.toIntArray()
+}
+
+class LinkedList
+
+//TODO: mplement LinkedList, concat
